3bab029396950d55264073cace49db18917487e8
dongsu <netfron@gmail.com>
2018-11-24 17:52:29 +0900

    delete

--- dist/src/controllers/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './todo.controller';
-export * from './todo-list.controller';
-export * from './todo-list-todo.controller';
-export * from './users.controller';
--- dist/src/models/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './todo.model';
-export * from './todo-list.model';
